3043a3f997fb7aafed99b357aa1e63a27b993621
Wu Xie <shiehng@gmail.com>
2019-10-31 21:33:46 +0800

    [-]删除重构

--- hustbook/blueprints/admin.py
+++
@@ -1,9 +0,0 @@
-'''
-@Description: 
-@Author: Wu Xie
-@Github: https://github.com/shiehng
-@Date: 2019-07-23 23:24:02
-'''
-from flask import Blueprint
-
-admin_bp = Blueprint('admin', __name__)
--- hustbook/blueprints/blog.py
+++
@@ -1,9 +0,0 @@
-'''
-@Description: 
-@Author: Wu Xie
-@Github: https://github.com/shiehng
-@Date: 2019-07-23 23:23:48
-'''
-from flask import Blueprint
-
-blog_bp = Blueprint('blog', __name__)
